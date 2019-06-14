82454ff6616b0a060d081af699948f0635de7ecb
aradzikowska12 <aradzikowska12@gmail.com>
2019-06-14 19:53:18 +0200

    8.4

---
+++ src/main/java/com/kodillaxxx/com/kodillaXXX/exception/Flight.java
@@ -0,0 +1,23 @@
+package com.kodillaxxx.com.kodillaXXX.exception;
+
+public class Flight {
+    String departureAirport;
+    String arrivalAirport;
+
+    public Flight(String departureAirport, String arrivalAirport) {
+        this.departureAirport = departureAirport;
+        this.arrivalAirport = arrivalAirport;
+    }
+
+    public Flight() {
+    }
+
+    public String getDepartureAirport() {
+        return departureAirport;
+    }
+
+    public String getArrivalAirport() {
+        return arrivalAirport;
+    }
+
+}
---
+++ src/main/java/com/kodillaxxx/com/kodillaXXX/exception/RouteNotFoundException.java
@@ -0,0 +1,7 @@
+package com.kodillaxxx.com.kodillaXXX.exception;
+
+public class RouteNotFoundException extends Exception {
+    public RouteNotFoundException(final String message) {
+        super(message);
+    }
+}
