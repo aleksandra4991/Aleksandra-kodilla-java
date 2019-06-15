15a98e3241514516cc64652ff446b149fd859aa0
aradzikowska12 <aradzikowska12@gmail.com>
2019-06-15 18:40:26 +0200

    13.1 commit

---
+++ src/main/java/com/kodillaxxx/com/kodillaXXX/good/patterns/StreamMain.java
@@ -0,0 +1,17 @@
+package com.kodillaxxx.com.kodillaXXX.good.patterns;
+
+import java.util.stream.Collectors;
+
+public class StreamMain {
+
+    public static void main (String args[]) {
+
+        String movies = MovieStore.getMovies().entrySet()
+                .stream()
+                .flatMap(stringListEntry -> stringListEntry.getValue().stream())
+                .collect(Collectors.joining("!"));
+
+        System.out.println(movies);
+    }
+
+}
