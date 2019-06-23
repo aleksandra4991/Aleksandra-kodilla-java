d44ebcac1067130b92439db4e6b0ddfb1462b82b
aradzikowska12 <aradzikowska12@gmail.com>
2019-06-23 20:31:26 +0200

    13.5 third part

--- src/main/java/com/kodillaxxx/com/kodillaXXX/good/patterns/FifthPartOfThirteenModule/Main.java
+++ src/main/java/com/kodillaxxx/com/kodillaXXX/good/patterns/FifthPartOfThirteenModule/Main.java
@@ -1,5 +1,8 @@
 package com.kodillaxxx.com.kodillaXXX.good.patterns.FifthPartOfThirteenModule;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -17,8 +20,25 @@ public class Main {
                 .map(flights -> flights.getFlightFrom().getCityFlightFrom())
                 .forEach(System.out::println);
 
-        System.out.println("Z Warszawy do Lublany,możesz polecieć z przesiadką w Pradze");
+        List<String> flightsFromWarsaw = CombinationOfFlightsFromFewCities.getList()
+                .stream()
+                .filter(e -> e.getFlightFrom().getCityFlightFrom().equals("Warsaw"))
+                .map(directFlightTo -> directFlightTo.getDirectFlightTo().getDirectCityFlightTo())
+                .collect(Collectors.toList());
+
+        List<String> flightsToLublana = CombinationOfFlightsFromFewCities.getList()
+                .stream()
+                .filter(e -> e.getDirectFlightTo().getDirectCityFlightTo().equals("Lublana"))
+                .map(flights -> flights.getFlightFrom().getCityFlightFrom())
+                .collect(Collectors.toList());
+
+        for (String flightFROMWarsaw :flightsFromWarsaw) {
+            for (String flightTOLublana : flightsToLublana) {
+                if (flightFROMWarsaw.equals(flightTOLublana)) {
+
+                    System.out.println("Z Warszawy do Lublany,możesz polecieć z przesiadką w: " + flightFROMWarsaw);
+                }
+            }
+        }
     }
-
 }
-
